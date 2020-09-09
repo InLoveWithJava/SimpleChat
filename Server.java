11e0baa07b9e1ada146bae9670b509e3ad13ee24
InLoveWithJava <javalang@list.ru>
2020-09-09 16:05:03 +0300

    #7 - lvl 7

--- Server.java
+++ Server.java
@@ -3,8 +3,12 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
     private static class Handler extends Thread {
         private Socket socket;
 
@@ -29,6 +33,16 @@ public class Server {
                 server.close();
             } catch (IOException ioException) {
                 System.out.println("Ошибка при закрытии серверного сокета.");
+            }
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        for (Map.Entry<String, Connection> element : connectionMap.entrySet()) {
+            try {
+                element.getValue().send(message);
+            } catch (IOException e) {
+                System.out.println("Ошибка при отправке сообщения");
             }
         }
     }
