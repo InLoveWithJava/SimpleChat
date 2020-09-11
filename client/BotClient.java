aa0fdb997c4189c29d00737d39c652f97f094f71
InLoveWithJava <javalang@list.ru>
2020-09-11 18:37:41 +0300

    #19 - lvl 19

--- client/BotClient.java
+++ client/BotClient.java
@@ -1,9 +1,105 @@
 package com.javarush.task.task30.task3008.client;
 
+import com.javarush.task.task30.task3008.ConsoleHelper;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class BotClient extends Client {
     public class BotSocketThread extends Client.SocketThread {
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+            super.clientMainLoop();
+        }
 
+        @Override
+        protected void processIncomingMessage(String message) {
+//            ConsoleHelper.writeMessage(message);
+//            String[] strings = message.split(": ");
+//            if (message == null || !message.contains(": ")) return;
+//            String name = strings[0].trim();
+//            String text = strings[1].trim();
+//            SimpleDateFormat simpleDateFormat = null;
+//            Calendar calendar = Calendar.getInstance();
+//            switch(text) {
+//                case "дата":
+//                    simpleDateFormat = new SimpleDateFormat("d.MM.YYYY");
+//                    break;
+//                case "день":
+//                    simpleDateFormat = new SimpleDateFormat("d");
+//                    break;
+//                case "месяц":
+//                    simpleDateFormat = new SimpleDateFormat("MMMM");
+//                    break;
+//                case "год":
+//                    simpleDateFormat = new SimpleDateFormat("YYYY");
+//                    break;
+//                case "время":
+//                    simpleDateFormat = new SimpleDateFormat("H:mm:ss");
+//                    break;
+//                case "час":
+//                    simpleDateFormat = new SimpleDateFormat("H");
+//                    break;
+//                case "минуты":
+//                    simpleDateFormat = new SimpleDateFormat("m");
+//                    break;
+//                case "секунды":
+//                    simpleDateFormat = new SimpleDateFormat("s");
+//                    break;
+//            }
+//            message = simpleDateFormat.format(calendar.getTime());
+//            if (!message.isEmpty())
+//                sendTextMessage("Информация для " + name + ": " + message);
+            ConsoleHelper.writeMessage(message);
+            if (message == null || !message.contains(": ")) return;
+            String[] strings = message.split(": ");
+            String userName = strings[0];
+            String text = strings[1];
+            SimpleDateFormat simpleDateFormat;
+            switch (text) {
+                case "дата":
+                    simpleDateFormat = new SimpleDateFormat("d.MM.YYYY");
+                    break;
+                case "день":
+                    simpleDateFormat = new SimpleDateFormat("d");
+                    break;
+                case "месяц":
+                    simpleDateFormat = new SimpleDateFormat("MMMM");
+                    break;
+                case "год":
+                    simpleDateFormat = new SimpleDateFormat("YYYY");
+                    break;
+                case "время":
+                    simpleDateFormat = new SimpleDateFormat("H:mm:ss");
+                    break;
+                case "час":
+                    simpleDateFormat = new SimpleDateFormat("H");
+                    break;
+                case "минуты":
+                    simpleDateFormat = new SimpleDateFormat("m");
+                    break;
+                case "секунды":
+                    simpleDateFormat = new SimpleDateFormat("s");
+                    break;
+                default:
+                    simpleDateFormat = null;
+                    break;
+            }
+
+            if (simpleDateFormat != null) {
+                Calendar calendar = Calendar.getInstance();
+                String result = "Информация для " + userName + ": ";
+                result += simpleDateFormat.format(calendar.getTime());
+                sendTextMessage(result);
+            }
+        }
     }
+
+
 
     @Override
     protected boolean shouldSendTextFromConsole() {
